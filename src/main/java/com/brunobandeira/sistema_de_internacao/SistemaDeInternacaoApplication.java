63015421fa67522c42a3ca75bda0154de906891a
Bruno <brunohenrique0665@gmail.com>
2024-06-09 20:55:21 -0300

    first commit

---
+++ src/main/java/com/brunobandeira/sistema_de_internacao/SistemaDeInternacaoApplication.java
@@ -0,0 +1,13 @@
+package com.brunobandeira.sistema_de_internacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeInternacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeInternacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunobandeira/sistema_de_internacao/SistemaDeInternacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunobandeira.sistema_de_internacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeInternacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
