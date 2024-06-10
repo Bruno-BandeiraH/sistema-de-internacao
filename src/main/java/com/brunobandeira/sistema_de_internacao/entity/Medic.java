2c1cebac32ed9978852ccf6222046632c6f4c57c
Bruno <brunohenrique0665@gmail.com>
2024-06-09 21:51:48 -0300

    creating patient and medic entities

---
+++ src/main/java/com/brunobandeira/sistema_de_internacao/entity/Medic.java
@@ -0,0 +1,76 @@
+package com.brunobandeira.sistema_de_internacao.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "medics")
+public class Medic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @NotBlank
+    @Column(name = "name")
+    private String fullName;
+    @NotBlank
+    private String speciality;
+    @Past
+    private LocalDate birthDate;
+    @Email
+    private String email;
+    @NotNull
+    @Min(11)
+    private int number;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
---
+++ src/main/java/com/brunobandeira/sistema_de_internacao/entity/Patient.java
@@ -0,0 +1,127 @@
+package com.brunobandeira.sistema_de_internacao.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @NotBlank
+    @Column(name = "name")
+    private String fullName;
+    @NotBlank
+    @Column(name = "father")
+    private String fatherName;
+    @NotBlank
+    @Column(name = "mother")
+    private String motherName;
+    @CPF
+    private String cpf;
+    @Past
+    private LocalDate birthDate;
+    @NotBlank
+    private String profession;
+    private String address;
+    @Email
+    private String email;
+    private String maritalStatus;
+    @NotNull
+    @Min(11)
+    private int number;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getMotherName() {
+        return motherName;
+    }
+
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
